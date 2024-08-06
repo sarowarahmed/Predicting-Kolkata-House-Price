33987c83bb2de1b2a793a07ea8cc32a3127c5197
Sarowar / <113256659+sarowarahmed@users.noreply.github.com>
2024-08-07 04:18:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 st.title('🏡Predicting Kolkata House Price')
 
-st.write('This app builds a machine learning model to predict the house price in kolkata')
+st.info('This app builds a machine learning model to predict the house price in kolkata')
 
-df = pd.read_csv('https://raw.githubusercontent.com/sarowarahmed/Predicting-Kolkata-House-Price/master/Kolkata%20House%20Price.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/sarowarahmed/Predicting-Kolkata-House-Price/master/Kolkata%20House%20Price.csv')
+  df
