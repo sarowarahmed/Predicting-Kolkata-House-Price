7ef9f752cc0778f98eeca5c1dae1732033e1dd3d
Sarowar / <113256659+sarowarahmed@users.noreply.github.com>
2024-08-06 23:22:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🏠Predicting-Kolkata-House-Price')
+st.title('🏡Predicting Kolkata House Price')
 
 st.write('This app builds a machine learning model to predict the house price in kolkata')
