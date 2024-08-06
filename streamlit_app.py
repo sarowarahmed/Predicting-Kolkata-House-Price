77ec6eadf161c924546e34c5f48a3be46aa1495f
Sarowar / <113256659+sarowarahmed@users.noreply.github.com>
2024-08-07 04:23:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,12 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/sarowarahmed/Predicting-Kolkata-House-Price/master/Kolkata%20House%20Price.csv')
   df
+
+  # Separate features and target
+  st.write('**X**')
+  X = df.drop(['Property_ID', 'PRICE(₹)'], axis=1)
+  X
+  st.write('**y**')
+  y = df['PRICE(₹)']
+  y
+ 
